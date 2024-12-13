3148f0afe20eb06ed88ddf829eb0d9bbb0818968
201804076 <127905757+201804076@users.noreply.github.com>
2024-12-13 16:36:47 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,10 +12,15 @@ export default function App() {
   return (
     <MedicationProvider>
       <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name="Home" component={Home} />
-          <Tab.Screen name="Medication" component={Medication} />
-          <Tab.Screen name="History" component={History} />
+        <Tab.Navigator
+        screenOptions={{
+          tabBarShowIcon: false, 
+          tabBarLabelStyle: { fontSize: 14, fontWeight: 'bold' },
+          tabBarStyle: { height: 60 },
+        }}>
+          <Tab.Screen name="홈" component={Home} />
+          <Tab.Screen name="약 추가" component={Medication} />
+          <Tab.Screen name="기록" component={History} />
         </Tab.Navigator>
       </NavigationContainer>
     </MedicationProvider>
